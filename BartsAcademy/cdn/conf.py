27b6976962f264ef193a8eccdd5c7e3cfa55ff54
Bretting <75083733+Bretting@users.noreply.github.com>
2023-06-12 00:55:03 +0200

    update spaces connection

--- BartsAcademy/cdn/conf.py
+++ BartsAcademy/cdn/conf.py
@@ -7,7 +7,7 @@ AWS_S3_ENDPOINT_URL="https://ams3.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
-AWS_LOCATION = "https://rhum-st-barth.ams3.digitaloceanspaces.com"
+AWS_LOCATION = "https://academy.ams3.digitaloceanspaces.com"
 
-DEFAULT_FILE_STORAGE = "rhum.cdn.backends.MediaRootS3Boto3Storage"
-STATICFILES_STORAGE = "rhum.cdn.backends.StaticRootS3Boto3Storage"
+DEFAULT_FILE_STORAGE = "BartsAcademy.cdn.backends.MediaRootS3Boto3Storage"
+STATICFILES_STORAGE = "BartsAcademy.cdn.backends.StaticRootS3Boto3Storage"
