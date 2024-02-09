171879b9669105d312dddc4aaf00b57130d44815
Bretting <75083733+Bretting@users.noreply.github.com>
2024-02-09 13:05:36 +0100

    mobile fixes

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -20,12 +20,26 @@ $(document).ready(function() {
 
   $(".owl-carousel").owlCarousel({
     center: true,
-    items:4,
+    items:5,
     loop:true,
     autoplay:true,
     autoplayTimeout:1500,
     autoplayHoverPause:true,
     lazyload: true,
+    margin:75,
+    responsive : {
+      // breakpoint from 0 up
+      0 : {
+          items:1,
+          nav:false,
+          margin:0
+      },
+      // breakpoint from 480 up
+      480 : {
+          items:3,
+          nav:false,
+      }
+    }
   });
 });
 
