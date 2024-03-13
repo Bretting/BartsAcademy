de436bc8d9d41f152c831a0397627e818f7d18dd
Bretting <75083733+Bretting@users.noreply.github.com>
2024-03-13 09:52:53 +0100

    redis test

--- BartsAcademy/settings.py
+++ BartsAcademy/settings.py
@@ -138,22 +138,24 @@ if POSTGRES_READY:
     }
 
 
-# REDIS_PASSWORD = os.environ.get("REDIS_SECRET_KEY")
+REDIS_PASSWORD = os.environ.get("REDIS_SECRET_KEY")
 
-# if not DEBUG:
-#     CACHES = {
-#         'default': { 
-#             'BACKEND': 'django_redis.cache.RedisCache',
-#             'LOCATION': f'redis://:@64.23.213.140:6379/0',
-#             'OPTIONS': {
-#                 'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#                 'PASSWORD': '{REDIS_PASSWORD}'
-#             },
-#         }
-#     }
 
-#     SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-#     SESSION_CACHE_ALIAS = "default"
+CACHES = {
+    'default': { 
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': f'redis://:@64.23.213.140:6379/0',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'PASSWORD': '{REDIS_PASSWORD}'
+        },
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
