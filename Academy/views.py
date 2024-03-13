07cbcc91c0c626cd68720940621894ce0a4949d5
Bretting <75083733+Bretting@users.noreply.github.com>
2024-03-13 09:46:01 +0100

    Redis test

--- Academy/views.py
+++ Academy/views.py
@@ -49,7 +49,6 @@ def main_view(request):
 
 #Main view
 @login_required
-@cache_page(60*30)
 def category_view(request: HtmxHttpRequest, tag=None) -> HttpResponse:
 
     random_picture = random.choice(CoreImages.objects.all())
