3790168a98cbd2ac4c095924d060f1643e70e502
Bretting <75083733+Bretting@users.noreply.github.com>
2024-05-14 23:23:54 +0200

    import fix

--- Academy/views.py
+++ Academy/views.py
@@ -717,8 +717,8 @@ def SKU_importer(request):
 
                 object.save()
                 return redirect(object.get_absolute_url())
-            except:
-                return HttpResponse('Something went wrong. Please doublecheck the excel-template')
+            except Exception as e:
+                return HttpResponse('Something went wrong. Please doublecheck the excel-template. ' + e)
         
     context = {
         'brands' : brands
