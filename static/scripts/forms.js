df59713e054e164fd9ee3981e0b38cc962f34dee
Bretting <75083733+Bretting@users.noreply.github.com>
2024-05-15 09:29:40 +0200

    Updates to scripts

---
+++ static/scripts/forms.js
@@ -0,0 +1,39 @@
+//Functions for adding image gallery formsets on CreateBlog page.
+let imageForm = document.querySelectorAll(".image-form")
+let container = document.querySelector("#formset")
+let addButton = document.querySelector("#add-form")
+let totalForms = document.querySelector("#id_blogimage_set-TOTAL_FORMS")
+
+let formNum = imageForm.length-1
+addButton.addEventListener('click', addForm)
+
+function addForm(e){
+    e.preventDefault()
+
+    let newForm = imageForm[0].cloneNode(true)
+    let formImageRegex = RegExp(`blogimage_set-(\\d){1}-image`,'g')
+    let formTextRegex = RegExp(`blogimage_set-(\\d){1}-text`,'g')
+
+    formNum++
+    console.log(formNum)
+    newForm.innerHTML = newForm.innerHTML.replace(formImageRegex, `blogimage_set-${formNum}-image`)
+    newForm.innerHTML = newForm.innerHTML.replace(formTextRegex, `blogimage_set-${formNum}-text`)
+
+    container.insertBefore(newForm, addButton)
+    
+    totalForms.setAttribute('value', `${formNum+1}`)
+}
+
+
+
+dselect(document.querySelector('#id_bottle_tag'), {
+    search: true
+});
+dselect(document.querySelector('#id_brand_tag'), {
+
+    search: true
+});
+dselect(document.querySelector('#id_category_tag'), {
+
+  search: true
+});
