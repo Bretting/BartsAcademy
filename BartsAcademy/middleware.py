9a74cdc02285651a29945c9f90b8d973aa8eddda
Bretting <75083733+Bretting@users.noreply.github.com>
2024-05-16 12:48:58 +0200

    crawler detector update

--- BartsAcademy/middleware.py
+++ BartsAcademy/middleware.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect
+from crawlerdetect import CrawlerDetect
+from user_agents import parse
 
 class AgeGateMiddleware:
     EXCLUDED_URLS = ['/agegate']
@@ -8,7 +10,17 @@ class AgeGateMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT')
+        crawler_detect = CrawlerDetect()
         
+        # Check if the user agent is Googlebot and set the session cookie
+        if crawler_detect.isCrawler(user_agent):
+            request.session['agegate'] = True
+
+        user_agent = parse(request.META.get('HTTP_USER_AGENT', ''))
+        if user_agent.is_bot:
+            request.session['agegate'] = True
+
         if (not request.session.get('agegate') and 
             request.path not in self.EXCLUDED_URLS and 
             request.path not in self.IGNORED_PATHS and
