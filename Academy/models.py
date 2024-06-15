3780b347b8824879bd835e479200ff2c2fc1bf2a
Bretting <75083733+Bretting@users.noreply.github.com>
2024-06-15 17:11:32 +0200

    Bottle sorting & partial template image fix

--- Academy/models.py
+++ Academy/models.py
@@ -202,7 +202,7 @@ class Bottle(models.Model):
         return (f"{self.brand} - {self.name}")
     
     class Meta:
-        ordering = ['sorting','category', 'name']
+        ordering = ['sorting','category','brand', 'name']
 
     def get_absolute_url(self):
         return reverse('Academy:bottle_detail', kwargs={'item':self.slug})
