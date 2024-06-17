c51451478d6c87b3c90ed9bc49262f2802fd42ab
Bretting <75083733+Bretting@users.noreply.github.com>
2024-06-17 20:26:03 +0200

    Owl carousel error

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,30 +1,5 @@
 //Select2
-$(document).ready(function() {
-  
-  $(".owl-carousel").owlCarousel({
-    center: true,
-    items:5,
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:1500,
-    autoplayHoverPause:true,
-    lazyload: true,
-    margin:75,
-    responsive : {
-      // breakpoint from 0 up
-      0 : {
-          items:1,
-          nav:false,
-          margin:0
-      },
-      // breakpoint from 480 up
-      480 : {
-          items:3,
-          nav:false,
-      }
-    }
-  });
-});
+
 
 
 //Functions for the Brand/Category filter
